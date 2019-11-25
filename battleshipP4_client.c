5eeafcad7471fc90cac1417fdb52adda86912f4a
elasticMongrel <33241429+elasticMongrel@users.noreply.github.com>
2019-11-24 20:17:29 -0500

    Update battleshipP4_client.c

--- battleshipP4_client.c
+++ battleshipP4_client.c
@@ -1,7 +1,128 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#define hit 'X'				//Defines 'X' as value of hit
+#define miss 'O'			//Defines 'O' as value of miss
+#define blank '-'			//Defines '-' as value of blank
+#define column 10			//Defines number of columns
+#define row 10				//Defines number of rows
+
+#define PORT "3490" // the port client will be connecting to
+#define MAXDATASIZE 100 // max number of bytes we can get at once
+
+struct node {
+	char ycoord;		//Letter (row)
+	char xcoord;		//Number (column)
+	char hitormiss[7];	//Hit or miss
+	char type[15];		//Ship type (if hit)
+	struct node *next;	//Pointer to next node
+};
+struct node *head, *tail;		//Initialize pointers to head and tail of linked list
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa) {
+  if (sa->sa_family == AF_INET) {
+    return &(((struct sockaddr_in*)sa)->sin_addr);
+  }
+  return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+void communicate_with_server(int sockfd) {
+  /*char buff[MAX];
+  int n;
+	printf("brandonß");
+  while(1) {
+    bzero(buff, sizeof(buff));
+    printf("Enter the string : ");
+    n = 0;
+    while ((buff[n++] = getchar()) != '\n');
+    write(sockfd, buff, sizeof(buff));
+    bzero(buff, sizeof(buff));
+    read(sockfd, buff, sizeof(buff));
+    printf("From server : %s", buff);
+    if ((strncmp(buff, "exit", 4)) == 0) {
+      printf("Client exit...\n");
+      break;
+    }
+  }*/
+}
+
+int connect_to_server(int *argc, char ***argv) {
+	int sockfd, numbytes;
+  char buf[MAXDATASIZE];
+  struct addrinfo hints, *servinfo, *p;
+  int rv;
+  char s[INET6_ADDRSTRLEN];
+  if (*argc != 2) {
+    fprintf(stderr,"usage: client hostname\n");
+    exit(1);
+  }
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  if ((rv = getaddrinfo((*argv)[1], PORT, &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
+  }
+
+  // loop through all the results and connect to the first we can
+  for(p = servinfo; p != NULL; p = p->ai_next) {
+    if ((sockfd = socket(p->ai_family, p->ai_socktype,
+      p->ai_protocol)) == -1) {
+      perror("client: socket");
+      continue;
+    }
+
+    if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+      perror("client: connect");
+      close(sockfd);
+      continue;
+    }
+
+    break;
+  }
+
+
+  if (p == NULL) {
+    fprintf(stderr, "client: failed to connect\n");
+    return 2;
+  }
+
+  inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), s, sizeof s);
+  printf("client: connecting to %s\n", s);
+  freeaddrinfo(servinfo); // all done with this structure
+  if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+  	perror("recv");
+    exit(1);
+  }
+	buf[numbytes] = '\0';
+  printf("client: received '%s'\n",buf);
+
+	if (send(sockfd, "poop", 4, 0) == -1)
+		perror("send");
+	printf("Sent poop;)\n");
+  //close(sockfd);
+
+  return sockfd;
+}
+
 /*
 Initializes all variables for the game and creates the game board.
 */
-char** initialize(char *stat, char let[], char num[], int *boats, char board[row][column], char **pt, struct node *h, struct node *t, struct node *nn) {
+char** initialize(char *stat, char let[], char num[], int *boats, char board[row][column], char **pt, struct node *h, struct node *t, struct node *nn, int *argc, char ***argv, int *socket_fd) {
 	h = NULL;		//Initialize head to null
 	t = NULL;		//Initialize tail to null
 	nn = NULL;		//Initialize a new node to null
@@ -231,6 +352,11 @@ char** initialize(char *stat, char let[], char num[], int *boats, char board[row
 			}
 		}
 	}
+
+	*socket_fd = connect_to_server(argc, argv);
+	printf("bye\n");
+	//communicate_with_server(sockfd);
+
 	return pt;		//Return value of pt
 }
 
@@ -275,4 +401,41 @@ void termination(char **pt, struct node *h, struct node *t, struct node *nn) {
 	free(h);		//Clears memory for the head of linked list
 	free(t);		//Clears memory for the tail of linked list
 	free(nn);		//Clears memory for the new node of linked list
+}
+
+/*
+Starts the program and runs functions.
+*/
+int main(int argc, char *argv[]) {
+	struct node *new_node;		//Node to take in data throughout Battleship
+	new_node = (struct node *)malloc(sizeof(struct node));		//Allocates memory for new node
+	char state;			//Initializes state to be blank
+	char letter[3];		//Letter of coordinate user inputs
+	char number[3];		//Number of coordinate user inputs
+	int ships;				//Number of spots left to hit
+	char gameboard[row][column];	//Initializes grid for board
+	char **boardpt;				//Board pointer
+	int socket_fd;
+	boardpt = initialize(&state, letter, number, &ships, gameboard, boardpt, head, tail, new_node, &argc, &argv, &socket_fd);
+	while(ships != 0) {		// Runs until count of ships reaches 0
+		input(letter, number);
+
+		if (send(socket_fd, letter, 3, 0) == -1) {
+			perror("send");
+			exit(1);
+		}
+		printf("Sent letter: %s...\n", letter);
+
+		if (send(socket_fd, number, 3, 0) == -1) {
+			perror("send");
+			exit(1);
+		}
+		printf("Sent number: %s...\n", number);
+		//update(&state, letter, number, &ships, gameboard, head, new_node);
+		//printupdate(&state, ships, gameboard);
+		//insert_node(&head, &tail, new_node);
+		ships--;
+		printf("evan\n");
+	}
+	termination(boardpt, head, tail, new_node);
 }
